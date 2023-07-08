839a361fa62b5dcd9a332192acc4816af00eb3a6
iarrans <isabelarrans@gmail.com>
2023-07-08 12:52:03 +0200

    DungeonItem base

---
+++ Assets/Scripts/DungeonItem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDungeonItem", menuName = "Dungeon Item")]
+public class DungeonItem : ScriptableObject
+{
+
+    public int id;
+
+    public Sprite itemIcon;
+
+    public GameObject prefab3DModel;
+
+    public enum ItemType{
+        LOOT,//0
+        ENEMY,//1
+        DECORATION//2
+    }
+}
