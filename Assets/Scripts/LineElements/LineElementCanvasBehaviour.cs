f9bf306ff0ba208e6f78e1f5513022ce55a425e1
iarrans <isabelarrans@gmail.com>
2023-07-09 12:51:07 +0200

    Canvas lineas

---
+++ Assets/Scripts/LineElements/LineElementCanvasBehaviour.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineElementCanvasBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Image lobby;
+    public Image danger;
+
+    public Image normalPlayer;
+    public Image speedRunner;
+
+    public Image endBackground;
+    public Image pendingBackground;
+    public Image mapBackground;
+
+    public Slider timer;
+
+    //Warnings
+    public void SetLobbyActive(bool entry)
+    {
+        lobby.gameObject.SetActive(entry);
+    }
+
+    public void SetDangerActive(bool entry)
+    {
+        danger.gameObject.SetActive(entry);
+    }
+
+
+    //Players
+    public void SetNormalPlayerActive(bool entry)
+    {
+        normalPlayer.gameObject.SetActive(entry);
+    }
+
+    public void SetSpeedRunnerPlayerActive(bool entry)
+    {
+        speedRunner.gameObject.SetActive(entry);
+    }
+
+    public void SetTimerActive(bool entry)
+    {
+        timer.enabled = entry;
+    }
+
+
+    //Backgrounds
+    public void SetMapBackgroundActive(bool entry)
+    {
+        mapBackground.gameObject.SetActive(entry);
+    }
+
+    public void SetEndBackgrounActive(bool entry)
+    {
+        endBackground.gameObject.SetActive(entry);
+    }
+
+    public void SetPendingBackgroundActive(bool entry)
+    {
+        pendingBackground.gameObject.SetActive(entry);
+    }
+
+
+    //Slider indicating time
+    public void ChangeTimerValue(float value) {
+        timer.value = value;
+    }
+
+}
